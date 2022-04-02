7e41b5466f3d19cfb711a15220ef746fc6c6e8d1
sonnt <thanhson.rf@gmail.com>
2022-04-02 22:21:06 +0700

    rename file, optimization performance

--- eventring.go
+++ eventring.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
-	"sync/atomic"
-	"unsafe"
+	"time"
 )
 
 var (
@@ -17,66 +16,29 @@ var (
 	ErrOverflow           = errors.New("overflow")
 )
 
-const (
-	mutexLocked = 1 << iota // mutex is locked
-	mutexWoken
-	mutexStarving
-	mutexWaiterShift = iota
-)
-
-// Mutex is a locker which supports TryLock.
-type Mutex struct {
-	sync.Mutex
-}
-
-func (m *Mutex) LockSmart() {
-	for {
-		if m.TryLock() {
-			return
-		}
-		runtime.Gosched()
-	}
-}
-
-func (m *Mutex) Count() int {
-	v := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
-	v = v >> mutexWaiterShift
-	v = v + (v & mutexLocked)
-	return int(v)
-}
-
-func (m *Mutex) IsLocked() bool {
-	state := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
-	return state&mutexLocked == mutexLocked
-}
-
-func (m *Mutex) IsWoken() bool {
-	state := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
-	return state&mutexWoken == mutexWoken
-}
-
-func (m *Mutex) IsStarving() bool {
-	state := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
-	return state&mutexStarving == mutexStarving
-}
-
 // RingBuffer is a circular buffer that implement io.ReaderWriter interface.
 type RingBuffer[T any] struct {
-	buf    []T
-	size   int
-	r      int // next position to read
-	w      int // next position to write
-	isFull bool
-	cond   *sync.Cond
+	buf              []T
+	size             int
+	maxsize          int
+	r                int // next position to read
+	w                int // next position to write
+	isFull           bool
+	lockwriteread    *sync.Cond
+	eventread        *sync.Cond
+	eventbroacastpop *sync.Cond
 }
 
 // New returns a new RingBuffer whose buffer has the given size.
 func NewRing[T any](size int) *RingBuffer[T] {
 
 	ringbuf := RingBuffer[T]{
-		buf:  make([]T, size),
-		size: size,
-		cond: sync.NewCond(new(Mutex)),
+		buf:              make([]T, size),
+		size:             size,
+		maxsize:          size,
+		lockwriteread:    sync.NewCond(new(sync.RWMutex)),
+		eventread:        sync.NewCond(new(sync.Mutex)),
+		eventbroacastpop: sync.NewCond(new(sync.Mutex)),
 	}
 	return &ringbuf
 }
@@ -86,7 +48,7 @@ func (r *RingBuffer[T]) String() string {
 }
 
 func (r *RingBuffer[T]) read(p []T) (n int, err error) {
-	if r.w == r.r && !r.isFull {
+	if r.isempty() {
 		return 0, ErrIsEmpty
 	}
 
@@ -121,7 +83,7 @@ func (r *RingBuffer[T]) read(p []T) (n int, err error) {
 }
 
 func (r *RingBuffer[T]) write(p []T) (n int, err error) {
-	if r.isFull {
+	if r.isfull() {
 		return 0, ErrIsFull
 	}
 
@@ -165,7 +127,7 @@ func (r *RingBuffer[T]) write(p []T) (n int, err error) {
 }
 
 func (r *RingBuffer[T]) writeElement(c T) error {
-	if r.w == r.r && r.isFull {
+	if r.isfull() {
 		return ErrIsFull
 	}
 	r.buf[r.w] = c
@@ -181,6 +143,11 @@ func (r *RingBuffer[T]) writeElement(c T) error {
 	return nil
 }
 
+func (r *RingBuffer[T]) sendeventread() {
+	r.eventread.Signal()           //for one wait
+	r.eventbroacastpop.Broadcast() //for everything need signal
+}
+
 // Read reads up to len(p) bytes into p. It returns the number of bytes read (0 <= n <= len(p)) and any error encountered. Even if Read returns n < len(p), it may use all of p as scratch space during the call. If some data is available but not len(p) bytes, Read conventionally returns what is available instead of waiting for more.
 // When Read encounters an error or end-of-file condition after successfully reading n > 0 bytes, it returns the number of bytes read. It may return the (non-nil) error from the same call or return the error (and n == 0) from a subsequent call.
 // Callers should always process the n > 0 bytes returned before considering the error err. Doing so correctly handles I/O errors that happen after reading some bytes and also both of the allowed EOF behaviors.
@@ -188,9 +155,10 @@ func (r *RingBuffer[T]) Read(p []T) (n int, err error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
-	r.cond.L.Lock()
+	r.lockwriteread.L.Lock()
 	n, err = r.read(p)
-	r.cond.L.Unlock()
+	r.lockwriteread.L.Unlock()
+	r.sendeventread()
 	return n, err
 }
 
@@ -198,25 +166,33 @@ func (r *RingBuffer[T]) ReadWait(p []T) (n int) {
 	if len(p) == 0 {
 		return 0
 	}
-	r.cond.L.Lock()
+	r.lockwriteread.L.Lock()
 	for len(p) > r.length() {
-		r.cond.Wait()
+		r.lockwriteread.Wait() //wait signal from writer
 	}
 	n, _ = r.read(p)
-	r.cond.L.Unlock()
+	r.lockwriteread.L.Unlock()
+	r.sendeventread()
 	return n
 }
 
-func (r *RingBuffer[T]) ReadAll() ([]T, error) {
-	bufsize := r.Length()
+func (r *RingBuffer[T]) ReadAll() (retval []T, err error) {
+	r.lockwriteread.L.Lock()
+	defer func() {
+		r.lockwriteread.L.Unlock()
+		if err == nil {
+			r.eventread.Broadcast()
+			r.eventbroacastpop.Broadcast()
+		}
+	}()
+	bufsize := r.length()
 	if bufsize == 0 {
-		return nil, ErrIsEmpty
+		err = ErrIsEmpty
+		return
 	}
-	p := make([]T, bufsize)
-	r.cond.L.Lock()
-	_, err := r.read(p)
-	r.cond.L.Unlock()
-	return p, err
+	retval = make([]T, bufsize)
+	_, err = r.read(retval)
+	return
 }
 
 // TryRead read up to len(p) elements into p like Read but it is not blocking.
@@ -225,14 +201,21 @@ func (r *RingBuffer[T]) TryRead(p []T) (n int, err error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
-	// r.cond.L.(*sync.Mutex).TryLock()
-	ok := r.cond.L.(*sync.Mutex).TryLock()
+	ok := r.lockwriteread.L.(*sync.Mutex).TryLock()
 	if !ok {
 		return 0, ErrAccuqireLock
 	}
 
 	n, err = r.read(p)
-	r.cond.L.Unlock()
+	r.lockwriteread.L.Unlock()
+	if err == nil {
+		if n != 0 {
+			r.eventbroacastpop.Broadcast()
+			for i := 0; i < n; i++ {
+				r.eventread.Signal()
+			}
+		}
+	}
 	return n, err
 }
 
@@ -248,9 +231,17 @@ func (r *RingBuffer[T]) TryReadAll() ([]T, error) {
 
 // Pop reads and returns the next element from the input or ErrIsEmpty.
 func (r *RingBuffer[T]) Pop() (b T, err error) {
-	r.cond.L.Lock()
-	if r.w == r.r && !r.isFull {
-		r.cond.L.Unlock()
+	r.lockwriteread.L.Lock()
+	b, err = r.readElement()
+	r.lockwriteread.L.Unlock()
+	if err == nil {
+		r.sendeventread()
+	}
+	return b, err
+}
+
+func (r *RingBuffer[T]) readElement() (b T, err error) {
+	if r.isempty() {
 		return *new(T), ErrIsEmpty
 	}
 	b = r.buf[r.r]
@@ -260,35 +251,47 @@ func (r *RingBuffer[T]) Pop() (b T, err error) {
 	}
 
 	r.isFull = false
-	r.cond.L.Unlock()
 	return b, err
 }
 
 func (r *RingBuffer[T]) PopWait() (b T) {
-	r.cond.L.Lock()
-	for r.w == r.r && !r.isFull {
-		r.cond.Wait()
+	r.lockwriteread.L.Lock()
+	for r.isempty() {
+		r.lockwriteread.Wait()
 	}
-	b = r.buf[r.r]
-	r.r++
-	if r.r == r.size {
-		r.r = 0
-	}
-
-	r.isFull = false
-	r.cond.L.Unlock()
+	b, _ = r.readElement() //sure is no err
+	r.lockwriteread.L.Unlock()
+	r.sendeventread()
 	return b
+}
+
+func (r *RingBuffer[T]) WaitUntilNotFull() {
+	r.lockwriteread.L.Lock()
+	if r.isfull() {
+		r.lockwriteread.L.Unlock() //unlock free lockwriteread
+		r.eventbroacastpop.L.Lock()
+		r.eventbroacastpop.Wait() //wait for any data to be read
+		r.eventbroacastpop.L.Unlock()
+		return
+	} else {
+		r.lockwriteread.L.Unlock()
+	}
 }
 
 // TryRead read up to len(p) elements into p like Read but it is not blocking.
 // If it has not succeeded to accquire the lock, it return 0 as n and ErrAccuqireLock.
 func (r *RingBuffer[T]) TryPop() (p T, err error) {
-	retT := make([]T, 1)
-	if _, err = r.TryRead(retT); err == nil && len(retT) == 1 {
-		return retT[0], nil
-	} else {
-		return retT[0], err
+	var zero T
+	ok := r.lockwriteread.L.(*sync.Mutex).TryLock()
+	if !ok {
+		return zero, ErrAccuqireLock
 	}
+	p, err = r.readElement()
+	r.lockwriteread.L.Unlock()
+	if err == nil {
+		r.sendeventread()
+	}
+	return
 }
 
 // Write writes len(p) bytes from p to the underlying buf.
@@ -299,13 +302,12 @@ func (r *RingBuffer[T]) Write(p []T) (n int, err error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
-	r.cond.L.Lock()
+	r.lockwriteread.L.Lock()
 	n, err = r.write(p)
+	r.lockwriteread.L.Unlock()
 	for i := 0; i < n; i++ {
-		r.cond.Signal()
+		r.lockwriteread.Signal()
 	}
-	r.cond.L.Unlock()
-
 	return n, err
 }
 
@@ -313,15 +315,15 @@ func (r *RingBuffer[T]) WriteWait(p []T) {
 	if len(p) == 0 {
 		return
 	}
-	r.cond.L.Lock()
+	r.lockwriteread.L.Lock()
 	for r.free() < len(p) {
-		r.cond.Wait()
+		r.lockwriteread.Wait()
 	}
 	n, _ := r.write(p)
+	r.lockwriteread.L.Unlock()
 	for i := 0; i < n; i++ {
-		r.cond.Signal()
+		r.lockwriteread.Signal()
 	}
-	r.cond.L.Unlock()
 
 	return
 }
@@ -332,67 +334,96 @@ func (r *RingBuffer[T]) TryWrite(p []T) (n int, err error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
-	ok := r.cond.L.(*sync.Mutex).TryLock()
+	ok := r.lockwriteread.L.(*sync.Mutex).TryLock()
 	if !ok {
 		return 0, ErrAccuqireLock
 	}
 
 	n, err = r.write(p)
-	r.cond.L.Unlock()
-
+	r.lockwriteread.L.Unlock()
+	for i := 0; i < n; i++ {
+		r.lockwriteread.Signal()
+	}
 	return n, err
 }
 
 // Push writes one element into buffer, and returns ErrIsFull if buffer is full.
 func (r *RingBuffer[T]) Push(c T) error {
-	r.cond.L.Lock()
+	r.lockwriteread.L.Lock()
 	err := r.writeElement(c)
-	r.cond.Signal()
-	r.cond.L.Unlock()
+	r.lockwriteread.L.Unlock()
+	r.lockwriteread.Signal()
 	return err
 }
 
 func (r *RingBuffer[T]) PushForce(c T) {
-	r.cond.L.Lock()
-	if r.w == r.r && r.isFull {
-		r.cond.L.Unlock()
-		r.Pop()
-		r.cond.L.Lock()
+	r.lockwriteread.L.Lock()
+	if r.isfull() {
+		r.readElement() // remove element
 	}
 	r.writeElement(c)
-	r.cond.Signal()
-	r.cond.L.Unlock()
+	r.lockwriteread.L.Unlock()
+	r.lockwriteread.Signal()
 	return
 }
 
+//Wait for buffer is not full then Push writes one element into buffer.
 func (r *RingBuffer[T]) PushWait(c T) {
-	r.cond.L.Lock()
+	r.lockwriteread.L.Lock()
 	for r.w == r.r && r.isFull {
-		r.cond.Wait()
+		r.lockwriteread.Wait()
 	}
 	r.writeElement(c)
-	r.cond.Signal()
-	r.cond.L.Unlock()
+	r.lockwriteread.L.Unlock()
+	r.lockwriteread.Signal()
+	return
+}
+
+//Wait for buffer is not full then Push writes one element into buffer.
+//Not recommended for use
+func (r *RingBuffer[T]) PushWaitTimeOut(c T, timeout time.Duration) (err error) {
+	timeoutAt := time.Now().Add(timeout)
+	stepSleep := timeout / time.Nanosecond / 10
+	for {
+		r.lockwriteread.L.Lock()
+		if r.isfull() {
+			r.lockwriteread.L.Unlock()
+			if time.Now().After(timeoutAt) {
+				return fmt.Errorf("Timeout")
+			} else {
+				runtime.Gosched()
+				time.Sleep(stepSleep)
+			}
+		} else {
+			break
+		}
+	}
+	r.writeElement(c)
+	r.lockwriteread.L.Unlock()
+	r.lockwriteread.Signal() //signal for write new data
 	return
 }
 
 // TrywriteElementElement writes one element into buffer without blocking.
 // If it has not succeeded to accquire the lock, it return ErrAccuqireLock.
 func (r *RingBuffer[T]) TryPush(c T) error {
-	ok := r.cond.L.(*sync.Mutex).TryLock()
+	ok := r.lockwriteread.L.(*sync.Mutex).TryLock()
 	if !ok {
 		return ErrAccuqireLock
 	}
 
 	err := r.writeElement(c)
-	r.cond.L.Unlock()
+	r.lockwriteread.L.Unlock()
+	if err == nil {
+		r.lockwriteread.Signal()
+	}
 	return err
 }
 
 // Length return the length of available read bytes.
 func (r *RingBuffer[T]) Length() int {
-	r.cond.L.Lock()
-	defer r.cond.L.Unlock()
+	r.lockwriteread.L.Lock()
+	defer r.lockwriteread.L.Unlock()
 	return r.length()
 }
 
@@ -418,11 +449,12 @@ func (r *RingBuffer[T]) Capacity() int {
 
 // Free returns the length of available bytes to write.
 func (r *RingBuffer[T]) Free() int {
-	r.cond.L.Lock()
-	defer r.cond.L.Unlock()
+	r.lockwriteread.L.Lock()
+	defer r.lockwriteread.L.Unlock()
 	return r.free()
 }
 
+// return number free elements
 func (r *RingBuffer[T]) free() int {
 	if r.w == r.r {
 		if r.isFull {
@@ -440,8 +472,8 @@ func (r *RingBuffer[T]) free() int {
 
 // Copy returns all available read elements. It does not move the read pointer and only copy the available data.
 func (r *RingBuffer[T]) Copy() []T {
-	r.cond.L.Lock()
-	defer r.cond.L.Unlock()
+	r.lockwriteread.L.Lock()
+	defer r.lockwriteread.L.Unlock()
 
 	if r.w == r.r {
 		if r.isFull {
@@ -476,23 +508,72 @@ func (r *RingBuffer[T]) Copy() []T {
 
 // IsFull returns this ringbuffer is full.
 func (r *RingBuffer[T]) IsFull() bool {
-	r.cond.L.Lock()
-	defer r.cond.L.Unlock()
-	return r.isFull
+	r.lockwriteread.L.Lock()
+	defer r.lockwriteread.L.Unlock()
+	return r.isfull()
+}
+
+// resize resizes the deque to fit exactly twice its current contents.  This is
+// used to grow the queue when it is full, and also to shrink it when it is
+// only a quarter full.
+func (r *RingBuffer[T]) Resize() (changed bool) {
+	r.lockwriteread.L.Lock()
+	defer r.lockwriteread.L.Unlock()
+	return r.resize()
+}
+
+func (r *RingBuffer[T]) resize() (changed bool) {
+
+	if r.size >= r.maxsize || r.size <= r.maxsize/8 {
+		return
+	}
+	var newsize int
+	lenbuff := r.length()
+	if lenbuff == 0 {
+		newsize = r.maxsize / 8 //Get 1/8 of max size
+	} else {
+		newsize = lenbuff + lenbuff/2 //Add 1/2 old size
+		if newsize > r.maxsize {
+			newsize = r.maxsize
+		}
+	}
+
+	if newsize <= r.size {
+		r.buf = r.buf[:newsize]
+		return //no need resize
+	}
+	newBuf := make([]T, newsize)
+	if r.isfull() {
+		r.w++
+		r.isFull = false
+	}
+	r.size = newsize
+	copy(newBuf, r.buf)
+	return true
 }
 
 // IsEmpty returns this ringbuffer is empty.
 func (r *RingBuffer[T]) IsEmpty() bool {
-	r.cond.L.Lock()
-	defer r.cond.L.Unlock()
+	r.lockwriteread.L.Lock()
+	defer r.lockwriteread.L.Unlock()
+	return r.isempty()
+}
+
+func (r *RingBuffer[T]) isempty() bool {
 	return !r.isFull && r.w == r.r
+}
+
+func (r *RingBuffer[T]) isfull() bool {
+	return r.isFull && r.w == r.r
 }
 
 // Reset the read pointer and writer pointer to zero.
 func (r *RingBuffer[T]) Reset() {
-	r.cond.L.Lock()
-	defer r.cond.L.Unlock()
-
+	r.lockwriteread.L.Lock()
+	defer r.lockwriteread.L.Unlock()
+	if r.length() != 0 {
+		r.sendeventread()
+	}
 	r.r = 0
 	r.w = 0
 	r.isFull = false
--- mutiplereader.go
+++ mutiplereader.go
@@ -2,7 +2,6 @@ package goring
 
 import (
 	"errors"
-	"runtime"
 	"sync"
 	"sync/atomic"
 )
@@ -12,53 +11,58 @@ var (
 	InvalidBufferSize = errors.New("buffer must be of size 2^n")
 )
 
-type RingTBuffer[T any] struct {
+type RingMutipleReader[T any] struct {
 	length            uint32
 	bitWiseLength     uint32
 	headPointer       uint32 // next position to write
 	maximumConsumerId uint32
 	maxConsumers      int
-	consumerLock      sync.Mutex
+	*sync.RWMutex
+	*sync.Cond
+	readEvent         *sync.Cond
 	buffer            []T
 	readerPointers    []uint32
 	readerActiveFlags []uint32
 }
 
 type Consumer[T any] struct {
-	ring *RingTBuffer[T]
+	ring *RingMutipleReader[T]
 	id   uint32
 }
 
-func CreateBuffer[T any](size uint32, maxConsumers uint32) (RingTBuffer[T], error) {
+func NewRingMutipleReader[T any](size uint32, maxConsumers uint32) (rmr *RingMutipleReader[T], err error) {
 
 	if size&(size-1) != 0 {
-		return RingTBuffer[T]{}, InvalidBufferSize
+		err = InvalidBufferSize
+		return
 	}
 
-	return RingTBuffer[T]{
+	rmr = &RingMutipleReader[T]{
 		buffer:            make([]T, size+1, size+1),
 		length:            size,
 		bitWiseLength:     size - 1,
 		headPointer:       0,
 		maximumConsumerId: 0,
 		maxConsumers:      int(maxConsumers),
-		consumerLock:      sync.Mutex{},
+		RWMutex:           &sync.RWMutex{},
 		readerPointers:    make([]uint32, maxConsumers),
 		readerActiveFlags: make([]uint32, maxConsumers),
-	}, nil
+	}
+	rmr.readEvent = sync.NewCond(&sync.Mutex{})
+	rmr.Cond = sync.NewCond(rmr.RWMutex)
+	return
 }
 
 /*
-CreateConsumer
+NewConsumer
 Create a consumer by assigning it the id of the first empty position in the consumerPosition array. A nil value represents
 an unclaimed/not used consumer.
 Locks can be used as it has no effect on read/write operations and is only to keep consumer consistency, thus the
 algorithm is still lockless For best performance, consumers should be preallocated before starting buffer operations
 */
-func (r *RingTBuffer[T]) CreateConsumer() (Consumer[T], error) {
-
-	r.consumerLock.Lock()
-	defer r.consumerLock.Unlock()
+func (r *RingMutipleReader[T]) NewConsumer() (Consumer[T], error) {
+	r.RLock()
+	defer r.RUnlock()
 
 	var newConsumerId = r.maxConsumers
 
@@ -86,10 +90,9 @@ func (r *RingTBuffer[T]) CreateConsumer() (Consumer[T], error) {
 	}, nil
 }
 
-func (r *RingTBuffer[T]) removeConsumer(consumerId uint32) {
-
-	r.consumerLock.Lock()
-	defer r.consumerLock.Unlock()
+func (r *RingMutipleReader[T]) removeConsumer(consumerId uint32) {
+	r.RLock()
+	defer r.RUnlock()
 
 	atomic.StoreUint32(&r.readerActiveFlags[consumerId], 0)
 	atomic.CompareAndSwapUint32(&r.maximumConsumerId, consumerId, r.maximumConsumerId-1)
@@ -103,7 +106,7 @@ func (consumer *Consumer[T]) Get() T {
 	return consumer.ring.readIndex(consumer.id)
 }
 
-func (r *RingTBuffer[T]) Write(value T) {
+func (r *RingMutipleReader[T]) Write(value T) {
 
 	var lastTailReaderPointerPosition uint32
 	var currentReadPosition uint32
@@ -132,22 +135,30 @@ func (r *RingTBuffer[T]) Write(value T) {
 		}
 
 		if lastTailReaderPointerPosition > r.headPointer {
-
 			r.buffer[r.headPointer&r.bitWiseLength] = value
 			atomic.AddUint32(&r.headPointer, 1)
+			r.Broadcast()
 			return
+		} else {
+			r.readEvent.L.Lock()
+			r.readEvent.Wait()
+			r.readEvent.L.Unlock()
 		}
-		runtime.Gosched()
+		// runtime.Gosched()
 	}
 }
 
-func (r *RingTBuffer[T]) readIndex(consumerId uint32) T {
+func (r *RingMutipleReader[T]) readIndex(consumerId uint32) T {
 
 	var newIndex = atomic.AddUint32(&r.readerPointers[consumerId], 1)
 
 	// yield until work is available
 	for newIndex >= atomic.LoadUint32(&r.headPointer) {
-		runtime.Gosched()
+		r.Lock()
+		r.Wait()
+		r.Unlock()
+		// runtime.Gosched()
 	}
+	r.readEvent.Broadcast()
 	return r.buffer[newIndex&r.bitWiseLength]
 }
